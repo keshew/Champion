307de2c5508888e520b7bf62a8fd8915004367ed
keshew <artyom.korotckov2017@yandex.ru>
2025-03-31 01:10:29 +0500

    Initial Commit

---
+++ Champion/ChampionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChampionApp.swift
+//  Champion
+//
+//  Created by Артём Коротков on 31.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChampionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
